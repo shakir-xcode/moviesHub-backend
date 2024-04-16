6ffc0142730f0b6a2833fabfa0f5ba83ece52be3
Shakir <shaker88999@gmail.com>
2024-04-16 12:32:10 +0530

    changed cors

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 const movieRoutes = require('./routes/movieRoutes');
+
+// Allow requests from specific origins
+const allowedOrigins = ['https://192.168.43.250:5173', 'https://movieshub08.netlify.app'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+// Use CORS middleware with custom options
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 
